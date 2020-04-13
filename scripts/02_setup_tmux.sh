95bc499e3816d1663d719388a931958d4de2d26b
Roman Handke <roman.handke@online.de>
2020-04-13 17:00:52 +0200

    Add tmux config

--- scripts/02_setup_tmux.sh
+++ scripts/02_setup_tmux.sh
@@ -4,7 +4,7 @@
                          
 #                        File Name     :  
 #                        Creation Date : 13.04.2020
-#                        Last Modified : Mo 13 Apr 2020 16:50:30 CEST
+#                        Last Modified : Mo 13 Apr 2020 16:59:05 CEST
 #                        Created By    : roman handke
                         
 ################################################################################
@@ -21,4 +21,4 @@ apt install -y tmux &> /dev/null || echo "[error] Could not install tmux"
 # Configure tmux
 ## Get user home directory
 USERNAME=$(grep 1000 /etc/passwd | awk 'BEGIN { FS = ":" } { print $1 }')
-echo cp ../configs/.tmux.conf "/home/${USERNAME}/"
+cp ../configs/.tmux.conf "/home/${USERNAME}/"
