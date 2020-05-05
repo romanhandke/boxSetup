82f2e7a0852add6f78363af212e8b96fe42decba
Roman Handke <roman.handke@online.de>
2020-05-05 20:23:37 +0200

    Fix ripgrep installation

--- scripts/01_install_packages.sh
+++ scripts/01_install_packages.sh
@@ -4,7 +4,7 @@
                          
 #                        File Name     :  
 #                        Creation Date : 13.04.2020
-#                        Last Modified : Di 05 Mai 2020 20:21:45 CEST
+#                        Last Modified : Di 05 Mai 2020 20:23:33 CEST
 #                        Created By    : roman handke
                         
 ################################################################################
@@ -36,7 +36,7 @@ WORKIN_DIRECTORY=$(pwd)
 
 installRipGrep() {
   info "Installing ripgrep"
-  curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb /tmp &> /dev/null || error "Could not download ripgrep"
+  curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb &> /dev/null || error "Could not download ripgrep"
   dpkg -i ripgrep_11.0.2_amd64.deb &> /dev/null || error "Could not install ripgrep"
 }
 
