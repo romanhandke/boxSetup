f75f8cde7332793d8a97ecb8d9a8f777effe52c5
Roman Handke <roman.handke@online.de>
2020-05-05 18:37:40 +0200

    Create node installation script

---
+++ scripts/20_setup_node.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+################################################################################
+                         
+#                        File Name     : 20_setup_node.sh
+#                        Creation Date : 05.05.2020
+#                        Last Modified : Di 05 Mai 2020 18:26:15 CEST
+#                        Created By    : roman handke
+                        
+################################################################################
+
+                              ##### PURPOSE #####
+
+                # Install some specific node versions using nvm
+
+################################################################################
+
+                             ##### FUNCTIONS #####
+
+installNvm() {
+  su - "${USERNAME}" -c "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash " || error "Could not install nvm"
+}
+
+installNode() {
+  su - "${USERNAME}" -c "nvm install ${1}"
+}
+
+################################################################################
+
+                             ##### VARIABLES #####
+
+NODE_VERSIONS=( \
+  "8.17.0" \
+  "10.20.1" \
+  "12.16.3" \
+)
+
+################################################################################
+
+##### SCRIPT #####
+
+installNvm
+reinitializeShell
+
+for NODE in "${NODE_VERSIONS[@]}" do
+  installNode "${NODE}"
+done
