81879c2462d83db891c3c17920f4ef0244137e18
Roman Handke <roman.handke@online.de>
2020-05-08 17:22:09 +0200

    Refactor php_utils installation

--- scripts/available/11_setup_php_utils.sh
+++ scripts/available/11_setup_php_utils.sh
@@ -4,7 +4,7 @@
                          
 #                        File Name     : 11_setup_php_utils.sh
 #                        Creation Date : 18.04.2020
-#                        Last Modified : Fr 08 Mai 2020 17:14:45 CEST
+#                        Last Modified : Fr 08 Mai 2020 17:21:49 CEST
 #                        Created By    : roman handke
                         
 ################################################################################
@@ -18,28 +18,13 @@
                              ##### FUNCTIONS #####
 
 installGlobalComposerModules() {
+  info "Installing ${2}" || info "Installing composer module"
   su - ${USERNAME} -c "composer global require ${1}" &> /dev/null || error "Could not install ${1}"
 }
-
-################################################################################
-
-                             ##### VARIABLES #####
-
-COMPOSER_MODULES=( \
-  "phpmd/phpmd" \
-  "squizlabs/php_codesniffer" \
-  "phpstan/phpstan" \
-  "psy/psysh:@stable" \
-)
 
 ################################################################################
 
                                ##### SCRIPT #####
 
 installPackage "composer"
-
-info "Installing php code quality tools"
-for MODULE in "${COMPOSER_MODULES[@]}"
-do
-  installGlobalComposerModules "${MODULE}"
-done
+installGlobalComposerModules "psy/psysh:@stable" "PsySh"
